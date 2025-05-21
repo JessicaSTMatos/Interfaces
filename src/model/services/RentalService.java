e5eff4cfbd39a157487e84598811d604400405fc
Jéssica dos Santos Turibo de Matos <86639430+JessicaSTMatos@users.noreply.github.com>
2025-05-21 08:05:30 -0300

    Interface TaxService

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -10,13 +10,13 @@ public class RentalService {
 	private Double pricePerHour;
 	private Double pricePerDay;
 
-	private BrazilTaxService taxService;
+	private TaxService taxService;
 
 	public RentalService() {
 
 	}
 
-	public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+	public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
 		this.pricePerHour = pricePerHour;
 		this.pricePerDay = pricePerDay;
 		this.taxService = taxService;
@@ -55,11 +55,11 @@ public class RentalService {
 		this.pricePerDay = pricePerDay;
 	}
 
-	public BrazilTaxService getTaxService() {
+	public TaxService getTaxService() {
 		return taxService;
 	}
 
-	public void setTaxService(BrazilTaxService taxService) {
+	public void setTaxService(TaxService taxService) {
 		this.taxService = taxService;
 	}
 
